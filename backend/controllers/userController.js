11887a851df17a7869adae01fb75e90a715d29e0
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-09-11 22:51:41 +0530

    validation sign up in server

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,28 @@ const userModel = require("../models/userModel.js");
 const userSignUpController = async (req, res) => {
   try {
     const { name, userName, email, password } = req.body;
+    // validation;
+    if (!name || !userName || !email || !password) {
+      return res.status(422).send({
+        success: false,
+        message: "please fill the all feild",
+      });
+    }
+    // existingUser with two feild with $or operator
+    const isUser = await userModel.findOne({
+      $or: [
+        {
+          email: email,
+        },
+        { userName: userName },
+      ],
+    });
+    if (isUser) {
+      return res.status(422).send({
+        success: false,
+        message: "user already exist",
+      });
+    }
     //user
     const user = new userModel({ name, userName, email, password });
     await user.save();
