233a9bdbfdfadbe7bcd84b07afca7285221b62e2
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-09-13 17:41:39 +0530

    conplete sign in api and test on postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,4 +48,41 @@ const userSignUpController = async (req, res) => {
   }
 };
 
-module.exports = { userSignUpController };
+//sign in
+const userSignInController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // validation
+    if (!email || !password) {
+      return res.status(422).send({
+        message: "fill valid email and password",
+        success: false,
+      });
+    }
+    //check email \\it is return a user data
+    const user = await userModel.findOne({ email }); // here may be i can wrong
+    if (!user) {
+      return res.status(422).send({
+        message: "please fill the valid email",
+        success: false,
+      });
+    }
+    //password check
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(422).send({
+        message: "invalid password",
+        success: false,
+      });
+    }
+
+    return res.status(200).send({
+      message: "login successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { userSignUpController, userSignInController };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { userSignUpController } = require("../controllers/userController");
+const {
+  userSignUpController,
+  userSignInController,
+} = require("../controllers/userController");
 
 const router = express.Router(); //router object
 
 //sign up user
 router.post("/sign-up", userSignUpController);
+
+//sign in user
+router.post("/sign-in", userSignInController);
 
 module.exports = router;
