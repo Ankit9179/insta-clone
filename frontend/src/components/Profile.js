468849cf7c2689899232a051c01dc40301c6ba9c
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-09-14 11:03:48 +0530

    profile design

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <>
+      <div className="frofile mx-auto">
+        <div className="flex w-[70%] mx-auto justify-evenly align-middle p-2 gap-6">
+          <div>
+            <img className="object-contain w-[120px] rounded-full mt-3" src="https://kidmatterscounseling.com/wp-content/uploads/2017/12/Lindsey-Holsten-500x500-Profile-Picture-compressed.jpg" alt="div" />
+          </div>
+          <div>
+            <h1 className="text-center text-2xl font-bold mb-6">Jojo gone</h1>
+            <div className="flex gap-3">
+              <p>29 Post</p>
+              <p>20 followers</p>
+              <p>30 following</p>
+            </div>
+          </div>
+        </div>
+        <hr className="w-[60%] mx-auto opacity-100 my-7" />
+        <div className="gallery  flex justify-center flex-wrap w-[100%] mx-auto">
+          <img className="object-contain w-[250px] m-5 " src="https://kidmatterscounseling.com/wp-content/uploads/2017/12/Lindsey-Holsten-500x500-Profile-Picture-compressed.jpg" alt="div" />
+          <img className="object-contain w-[250px] m-5 " src="https://kidmatterscounseling.com/wp-content/uploads/2017/12/Lindsey-Holsten-500x500-Profile-Picture-compressed.jpg" alt="div" />
+          <img className="object-contain w-[250px] m-5 " src="https://kidmatterscounseling.com/wp-content/uploads/2017/12/Lindsey-Holsten-500x500-Profile-Picture-compressed.jpg" alt="div" />
+          <img className="object-contain w-[250px] m-5 " src="https://kidmatterscounseling.com/wp-content/uploads/2017/12/Lindsey-Holsten-500x500-Profile-Picture-compressed.jpg" alt="div" />
+          <img className="object-contain w-[250px] m-5 " src="https://kidmatterscounseling.com/wp-content/uploads/2017/12/Lindsey-Holsten-500x500-Profile-Picture-compressed.jpg" alt="div" />
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default Profile;
